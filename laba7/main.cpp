0fd4ce5cc705fef6dde103284e93f345420c2f83
viperrvenom <nestvse22@gmail.com>
2024-12-21 02:22:31 +0200

    red

--- laba7/main.cpp
+++ laba7/main.cpp
@@ -1,36 +1,12 @@
-#include <iostream>
-#include <vector>
 #include "functions.hpp"
 
 int main() {
+    std::vector<std::vector<int>> matrix;
     int n;
-    std::cin >> n;
-    
-    std::vector<std::vector<int>> matrix(n, std::vector<int>(n));
-    int maxElem = INT_MIN;
-    
-    // чтение матрицы и поиск максимального элемента
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            std::cin >> matrix[i][j];
-            if (matrix[i][j] > maxElem) {
-                maxElem = matrix[i][j];
-            }
-        }
-    }
-    
-    // если в матрице есть еще один элемент, равный максимальному, сортируем строки
-    if (hasDuplicateMax(matrix, maxElem)) {
-        sortMatrixByPrimeCount(matrix, n);
-    }
-    
-    // выводим матрицу (возможно, отсортированную)
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            std::cout << matrix[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+
+    MatrixOperations::readMatrix(matrix, n);
+    MatrixOperations::processMatrix(matrix, n);
+    MatrixOperations::writeMatrix(matrix, n);
 
     return 0;
 }
